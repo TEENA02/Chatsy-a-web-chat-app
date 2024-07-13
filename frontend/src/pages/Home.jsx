ec39de4d3c8b1f08e14fd93fd714b0ab8d4d8309
TEENA02 <130280380+TEENA02@users.noreply.github.com>
2024-07-13 19:27:01 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import MessageContainer from "../components/messages/MessageContainer";
 const Home = () => {
   return (
     <div
-      className="flex h-[400px] sm:h-[450px] md:h-[550px]  bg-gray-100 rounded-md overflow-hidden 
+      className="flex  sm:h-[450px] md:h-[550px]  bg-gray-100 rounded-md overflow-hidden 
        bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border border-gray-100"
     >
       <Sidebar />
