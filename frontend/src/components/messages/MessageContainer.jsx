8d03764fd00cabacf13a35fc3e3a67eb61f5f6a6
TEENA02 <130280380+TEENA02@users.noreply.github.com>
2024-07-13 19:24:37 +0530

    Update MessageContainer.jsx

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -13,7 +13,7 @@ const MessageContainer = () => {
     }
   },[setSelectedConversation])
   return (
-    <div className="md:min-w-[450px] flex flex-col  h-[400px] sm:h-[450px] md:h-[550px]  ">
+    <div className="md:min-w-[200px] flex flex-col   ">
       {!selectedConversation ? (
         <NoChatSelected />
       ) : (
