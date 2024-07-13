b56ae28c26c22fbf7a37c0ddc9502824f0bf7e4b
TEENA02 <teenasajwan09@gmail.com>
2024-07-13 18:44:43 +0530

    connecting front and back ends

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
-
+import path from "path"
 
 //----------------Routes-------------------------
 import authRoutes from './routes/authroutes.js'
@@ -16,6 +16,7 @@ dotenv.config();
 //we cant directly get env variables
 const PORT=process.env.PORT || 5000;
 
+const __dirname=path.resolve()
 //middle ware
 app.use(express.json()); //to parse the incoming request with json payloads from(req.body)
 //to access the cookie
@@ -24,6 +25,11 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 // app.get("/",(req,res)=>{
 //     //route routr localhost:5000
 //     res.send("hello world!!!")
